50b335083daf043e0db915981129a9d7fe865ab4
Victor Yuki <victorstti@outlook.com>
2018-12-28 15:59:21 -0200

    Adicionar arquivos de projeto.

---
+++ ProjectSaleMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace ProjectSaleMVC.Models {
+    public class ErrorViewModel {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

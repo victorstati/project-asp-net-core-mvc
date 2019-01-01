6fed15d3b91efed65e44eb40e32a2dc9b6431bc6
Victor Yuki <victorstti@outlook.com>
2019-01-01 13:47:22 -0200

    MySQL adaptation and first migration

--- ProjectSaleMVC/Startup.cs
+++ ProjectSaleMVC/Startup.cs
@@ -32,7 +32,8 @@ namespace ProjectSaleMVC {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
     services.AddDbContext<ProjectSaleMVCContext>(options =>
-            options.UseSqlServer(Configuration.GetConnectionString("ProjectSaleMVCContext")));
+             options.UseMySql(Configuration.GetConnectionString("ProjectSaleMVCContext"), builder => 
+             builder.MigrationsAssembly("ProjectSaleMVC")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

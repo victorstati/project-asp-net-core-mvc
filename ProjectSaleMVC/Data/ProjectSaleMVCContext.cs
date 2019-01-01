e14f88cb7e3f0e7d40b472eebff7c59e3ff53db2
Victor Yuki <victorstti@outlook.com>
2019-01-01 10:36:10 -0200

    CRUD Scaffolding

---
+++ ProjectSaleMVC/Data/ProjectSaleMVCContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProjectSaleMVC.Models
+{
+    public class ProjectSaleMVCContext : DbContext
+    {
+        public ProjectSaleMVCContext (DbContextOptions<ProjectSaleMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjectSaleMVC.Models.Department> Department { get; set; }
+    }
+}

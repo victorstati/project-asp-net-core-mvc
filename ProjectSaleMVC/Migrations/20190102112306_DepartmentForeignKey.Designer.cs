682cfb91409833c73de95e753f8bff656a1942c4
Victor Yuki <victorstti@outlook.com>
2019-01-02 09:29:31 -0200

    Foreign key not null (referential integrity)

--- ProjectSaleMVC/Migrations/20190102112306_DepartmentForeignKey.Designer.cs
+++ ProjectSaleMVC/Migrations/20190102112306_DepartmentForeignKey.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProjectSaleMVC.Models;
 
 namespace ProjectSaleMVC.Migrations
 {
     [DbContext(typeof(ProjectSaleMVCContext))]
-    partial class ProjectSaleMVCContextModelSnapshot : ModelSnapshot
+    [Migration("20190102112306_DepartmentForeignKey")]
+    partial class DepartmentForeignKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

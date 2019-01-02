6e0953014c7e3b2562585aa0eed93baa36919a8f
Victor Yuki <victorstti@outlook.com>
2019-01-02 15:21:28 -0200

    Implementing grouping search

--- ProjectSaleMVC/Services/SalesRecordServices.cs
+++ ProjectSaleMVC/Services/SalesRecordServices.cs
@@ -33,5 +33,24 @@ namespace ProjectSaleMVC.Services
                 .OrderByDescending(x => x.Date)
                 .ToListAsync();
         }
+
+        public async Task<List<IGrouping<Department, SalesRecord>>> FindByDateGroupingAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+            return await result
+                .Include(x => x.Seller)//inner join com seller
+                .Include(x => x.Seller.Department)//inner join com department
+                .OrderByDescending(x => x.Date)
+                .GroupBy(x => x.Seller.Department)
+                .ToListAsync();
+        }
     }
 }

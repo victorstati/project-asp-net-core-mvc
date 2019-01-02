c2f7d975627b957ecb647b9ac2ddeed153fbc245
Victor Yuki <victorstti@outlook.com>
2019-01-02 11:47:35 -0200

    App locale, number and date formatting

--- ProjectSaleMVC/Startup.cs
+++ ProjectSaleMVC/Startup.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Globalization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
@@ -14,17 +16,22 @@ using ProjectSaleMVC.Models;
 using ProjectSaleMVC.Data;
 using ProjectSaleMVC.Services;
 
-namespace ProjectSaleMVC {
-    public class Startup {
-        public Startup(IConfiguration configuration) {
+namespace ProjectSaleMVC
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services) {
-            services.Configure<CookiePolicyOptions>(options => {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
@@ -33,9 +40,9 @@ namespace ProjectSaleMVC {
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-    services.AddDbContext<ProjectSaleMVCContext>(options =>
-             options.UseMySql(Configuration.GetConnectionString("ProjectSaleMVCContext"), builder => 
-             builder.MigrationsAssembly("ProjectSaleMVC")));
+            services.AddDbContext<ProjectSaleMVCContext>(options =>
+                     options.UseMySql(Configuration.GetConnectionString("ProjectSaleMVCContext"), builder =>
+                     builder.MigrationsAssembly("ProjectSaleMVC")));
 
             services.AddScoped<SeedingService>();//registra o serviço na injeção de dependência da aplicação
             services.AddScoped<SellerService>();
@@ -43,11 +50,25 @@ namespace ProjectSaleMVC {
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService) {
-            if (env.IsDevelopment()) {
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
+        {
+            var enUS = new CultureInfo("en-US");
+            var localizationOptions = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(enUS),
+                SupportedCultures = new List<CultureInfo> { enUS },
+                SupportedUICultures = new List<CultureInfo> { enUS }
+            };
+
+            app.UseRequestLocalization(localizationOptions);
+
+            if (env.IsDevelopment())
+            {
                 app.UseDeveloperExceptionPage();
                 seedingService.Seed();
-            } else {
+            }
+            else
+            {
                 app.UseExceptionHandler("/Home/Error");
                 app.UseHsts();
             }
@@ -56,7 +77,8 @@ namespace ProjectSaleMVC {
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
-            app.UseMvc(routes => {
+            app.UseMvc(routes =>
+            {
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");

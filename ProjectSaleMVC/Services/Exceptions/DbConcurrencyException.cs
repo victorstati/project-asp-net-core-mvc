ca2a453a5cb16571e0cd4c5ed1b96f7a6b5ad297
Victor Yuki <victorstti@outlook.com>
2019-01-02 11:16:43 -0200

    Update seller and custom service exception

---
+++ ProjectSaleMVC/Services/Exceptions/DbConcurrencyException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ProjectSaleMVC.Services.Exceptions
+{
+    public class DbConcurrencyException : ApplicationException
+    {
+        public DbConcurrencyException(string message) : base(message)
+        {
+        }
+    }
+}
--- ProjectSaleMVC/Services/SellerService.cs
+++ ProjectSaleMVC/Services/SellerService.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using ProjectSaleMVC.Services.Exceptions;
 
 namespace ProjectSaleMVC.Services
 {
@@ -38,6 +39,23 @@ namespace ProjectSaleMVC.Services
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
+        }
+
+        public void Update(Seller obj)
+        {
+            if (!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id not found");
+            }
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch(DbUpdateConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
         }
     }
 }

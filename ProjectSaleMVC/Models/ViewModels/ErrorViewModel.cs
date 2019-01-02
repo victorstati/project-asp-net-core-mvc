cc0b2342a9dea787da849408d2a1fa187927b836
Victor Yuki <victorstti@outlook.com>
2019-01-02 11:32:34 -0200

    Returning custom error page

--- ProjectSaleMVC/Models/ViewModels/ErrorViewModel.cs
+++ ProjectSaleMVC/Models/ViewModels/ErrorViewModel.cs
@@ -5,6 +5,7 @@ namespace ProjectSaleMVC.Models.ViewModels
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
+        public string Message { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
